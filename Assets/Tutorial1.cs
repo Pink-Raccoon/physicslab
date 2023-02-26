d7ceeeef88b62c25e91ee57717407c7ea8f2b260
Asha Schwegler <schweglerasha@gmail.com>
2023-02-26 18:47:08 +0100

    tutorial 1

---
+++ Assets/Tutorial1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This prints phrase "Hello World" to the console every frame.
+        print("Hello World");
+    }
+}
